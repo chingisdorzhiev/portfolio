d0c3f46d506ee445a8ea887380d14f6c8689cf79
chingisdorzhiev <132899348+chingisdorzhiev@users.noreply.github.com>
2023-09-20 20:59:17 +0800

    bix fugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import { HashRouter } from "react-router-dom";
 import Body from "./Components/Structure/Body/Body";
 import ScrollToTop from "./Components/ScrollTop";
 import { Provider } from "react-redux";
--- src/index.js
+++ src/index.js
@@ -2,24 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  },
-]);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
