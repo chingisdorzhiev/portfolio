80d18b11c6c86a8e7f5068d07bf078559fd6e790
chingisdorzhiev <132899348+chingisdorzhiev@users.noreply.github.com>
2023-09-20 19:23:40 +0800

    fix bug

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import HeaderContainer from "./Components/Structure/Header/HeaderContainer";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <div className="App">
           <ScrollToTop />
